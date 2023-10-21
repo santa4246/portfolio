50ab99b9e4edbc405183bf74b46ab915d1fdb736
santa4246 <santa4246@naver.com>
2023-10-21 16:01:58 +0900

    js 기능추가 (Project, Menu )

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ document.addEventListener('scroll', () => {
   } else{
     header.classList.remove('header--dark')
   }
-})
+});
 
 // Home 섹션을 아래로 스크롤시 투명하게 처리
 const home = document.querySelector('.home__container');
@@ -18,7 +18,7 @@ const homeHeight = home.offsetHeight;
 
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight
-})
+});
 
 // 현재 화면이 Home 섹션일 때 Arrow up 버튼 투명하게 처리
 const arrowUp = document.querySelector('.arrow-up');
@@ -33,7 +33,7 @@ document.addEventListener('scroll', () => {
   } else {
     arrowUp.style.opacity = 0;
   }
-})
+});
 
 // Navbar 토글 버튼 클릭
 const navbarMenu = document.querySelector('.header__menu');
@@ -41,9 +41,9 @@ const navbarToggle = document.querySelector('.header__toggle');
 
 navbarToggle.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
-})
+});
 
 // Navbar 메뉴 클릭시 메뉴를 자동으로 닫기
 navbarMenu.addEventListener('click', () => {
   navbarMenu.classList.remove('open');
-})
+});
--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// Project 필터링
+const categories = document.querySelector('.categories');
+const project = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null){
+    return;
+  }
+
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target){
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter){
+  project.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type){
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  
+  projectsContainer.classList.add('anim-out');
+  setTimeout(()=>{
+    projectsContainer.classList.remove('anim-out');
+  }, 250)
+}
